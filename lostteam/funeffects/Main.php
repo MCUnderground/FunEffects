834b078ac700ed5c3612c76712452a520f7bba88
MCUnderground <daniel.barinak@gmail.com>
2016-02-09 22:13:58 +0100

    Added whole new code.

--- lostteam/funeffects/Main.php
+++ lostteam/funeffects/Main.php
@@ -1,3 +1,54 @@
-?<php
+<?php
 
-namespace: lostteam\funeffects;
+namespace lostteam\funeffects;
+
+/*
+<?php
+
+namespace lostteam\funeffects;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+use pocketmine\Server;
+use pocketmine\utils\TextFormat;
+use pocketmine\event\Listener;
+
+class Main extends PluginBase implements Listener {
+
+    public function onEnable()
+{
+        $this->getLogger()->info("[FunEffects] Loaded!");
+    }
+
+
+
+    public function onCommand(CommandSender $sender, Command $command, $label, array $args)
+{
+        switch($command->getName()){
+            case "flash":
+                if (isset($args[0])) {
+                    $sender->sendMessage("Use /flash");
+                }
+                else {
+                    $effect = Effect::getEffect(1); 
+        $effect->setVisible(flase); 
+        $effect->setDuration(500); 
+                }
+                break;
+        }
+
+    }
+
+
+
+
+    public function onDisable(){
+        $this->getLogger()->info("[FunEffects] Disabled!");
+
+    }
+
+}
+
+
+*/

b6dfe7bc8ecbe0b06fda0762aaabf2329d3594e5
TylerGames <tylergamesleet@gmail.com>
2016-02-10 12:27:13 -0600

    Added Cool Text B)

--- src/lostteam/funeffects/Main.php
+++ src/lostteam/funeffects/Main.php
@@ -1,5 +1,13 @@
 <?php
 
+/*
+ *  _              _  _____                    
+| |    ___  ___| ||_   _|__  __ _ _ __ ___  
+| |   / _ \/ __| __|| |/ _ \/ _` | '_ ` _ \ 
+| |__| (_) \__ \ |_ | |  __/ (_| | | | | | |
+|_____\___/|___/\__||_|\___|\__,_|_| |_| |_|
+ */
+
 namespace lostteam\funeffects;
 use pocketmine\plugin\PluginBase;
 use pocketmine\command\CommandSender;
@@ -7,7 +15,6 @@ use pocketmine\command\Command;
 use pocketmine\Server;
 use pocketmine\utils\TextFormat;
 use pocketmine\event\Listener;
-
 class Main extends PluginBase implements Listener {
     public function onEnable()
 {

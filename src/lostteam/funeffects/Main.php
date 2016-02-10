2b898f50fcb2ad43fea8476ff2124ec196a5cb41
TylerGames <tylergamesleet@gmail.com>
2016-02-10 10:15:52 -0600

    Fixed Errors and added "/snail" change /snail to whatever you want I just randomly just came up with that
    Read lel

--- src/lostteam/funeffects/Main.php
+++ src/lostteam/funeffects/Main.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace lostteam\funeffects;
-
 use pocketmine\plugin\PluginBase;
 use pocketmine\command\CommandSender;
 use pocketmine\command\Command;
@@ -10,14 +9,10 @@ use pocketmine\utils\TextFormat;
 use pocketmine\event\Listener;
 
 class Main extends PluginBase implements Listener {
-
     public function onEnable()
 {
         $this->getLogger()->info("[FunEffects] Loaded!");
     }
-
-
-
     public function onCommand(CommandSender $sender, Command $command, $label, array $args)
 {
         switch($command->getName()){
@@ -26,55 +21,56 @@ class Main extends PluginBase implements Listener {
                     $sender->sendMessage("Use /flash");
                 }
                 else {
-                    $effect = Effect::getEffect(1); 
-$effect->setVisible(false); 
-$effect->setDuration(500); 
+                    $effect = Effect::getEffect(1); 
+$effect->setVisible(false); 
+$effect->setDuration(500); 
         $sender->sendPopup("You got fast!");
                 }
       
-
            case "sick":
-           if (isset($args[0])) {
+           if (!isset($args[0])) {
                     $sender->sendMessage("Use /sick");
                 }
                 else {
-                    $effect = Effect::getEffect(9); 
-$effect->setVisible(false); 
-$effect->setDuration(500); 
+                    $effect = Effect::getEffect(9); 
+$effect->setVisible(false); 
+$effect->setDuration(500); 
         $sender->sendPopup("You got sick!");
                 }
           case "heal":
-           if (isset($args[0])) {
+           if (!isset($args[0])) {
                     $sender->sendMessage("Use /heal");
                 }
                 else {
-                    $effect = Effect::getEffect(10); 
+                    $effect = Effect::getEffect(10); 
 $effect->setVisible(false);
-$effect->setDuration(500); 
+$effect->setDuration(500); 
         $sender->sendPopup("You are healing!");
                 }
                 case "hulk":
-           if (isset($args[0])) {
+           if (!isset($args[0])) {
                     $sender->sendMessage("Use /hulk");
                 }
                 else {
-                    $effect = Effect::getEffect(5); 
+                    $effect = Effect::getEffect(5); 
 $effect->setVisible(false);
-$effect->setDuration(500); 
+$effect->setDuration(500); 
         $sender->sendPopup("You are strong!");
                 }
+            case "snail"
+           if(!isset($args[0])){
+                    $sender->sendMessage("Use /snail");
+		        }
+				else {
+					$effect = Effect::getEffect(2);
+$effect->setVisible(false);
+$effect->setDuration(500);
+        $sender->sendPopup("You are slow!");
+				}
          break;
-
         }
-
     }
-
-
-
-
     public function onDisable(){
         $this->getLogger()->info("[FunEffects] Disabled!");
-
     }
-
 }
